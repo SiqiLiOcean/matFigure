a760f13475e42de089736605431ec7b813ca66b7
Siqi Li <1990lisiqi@gmail.com>
2022-12-16 11:02:05 -0500

    Update on 2022-12-16

--- mf_background.m
+++ mf_background.m
@@ -14,18 +14,21 @@
 function h = mf_background(ax, varargin)
 
 varargin = read_varargin(varargin, {'Color'}, {[222,184,135]/255});
+varargin = read_varargin(varargin, {'xlims'}, {xlim(ax)});
+varargin = read_varargin(varargin, {'ylims'}, {ylim(ax)});
 
-if isempty(varargin)
-    xlims = xlim(ax);
-    ylims = ylim(ax);
-else
-    xlims = varargin{1};
-    ylims = varargin{2};
-end
+
+% % if isempty(varargin)
+% %     xlims = xlim(ax);
+% %     ylims = ylim(ax);
+% % else
+% %     xlims = varargin{1};
+% %     ylims = varargin{2};
+% % end
 
  
-px = [xlims(1) xlims(2) xlims(2) xlims(1) xlims(1)];
-py = [ylims(1) ylims(1) ylims(2) ylims(2) ylims(1)];
+px = xlims([1 2 2 1 1]);
+py = ylims([1 1 2 2 1]);
 
 h = patch(ax, px, py, 1, 'FaceColor', Color);
 
